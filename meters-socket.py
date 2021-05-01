ff47417b4b61fc740150d45ac0321470dde73e30
tgalarneau <82619105+tgalarneau@users.noreply.github.com>
2021-05-01 12:46:15 -1000

    Add files via upload

--- meters-socket.py
+++ meters-socket.py
@@ -46,12 +46,13 @@
 #	Example:
 #	b'\xe0\xff\x0f\xc8;X\\A\xd7;\\A\x8a\xb1\x90=\xe2\x14y?B\xd7\xcf\xc0Lk\xfb\xff\xff\xff\xff\xff\x93\xa7\xff\xff\xff\xff\xff\xff\x0eS\x85\x00 \xcen\x10i\x85\xc4A'
 #
-
 import argparse
 import struct
 import time
 import sys
 import signal
+import atexit
+import socket
 from bluepy.btle import Peripheral, BTLEException
 
 # Slurp up command line arguments
@@ -60,56 +61,84 @@ group = parser.add_mutually_exclusive_group()
 parser.add_argument("-b", "--BLEaddress", help="BT DCPM BLE Address", required=True)
 parser.add_argument("-i", "--interval", type=int, help="Interval query", required=True)
 parser.add_argument("-m", "--meter", help="Meter type", required=True)
-parser.add_argument("-v", "--verbose", help="debug output", action="store_true")
-
 args = parser.parse_args()
-
-#The file handler for the Telegraf process.
-telegraf_socket = "/tmp/telegraf.sock"
-
-# Connection to Telegraf, over a network socket.
-#sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-#sock.connect(telegraf_socket)
-
-# Connect to thornwave. Try twice, then fail 
-try:
-  p = Peripheral(args.BLEaddress, addrType="random")
-
-except BTLEException as ex:
-  if args.verbose:
-    print("Read failed. ", ex)
-  time.sleep(10)
-  try:
-     p = Peripheral(args.BLEaddress, addrType="random")
-  except:
-     if args.verbose:
-       print("Read failed. ", ex)
-     exit
-else:
-  result=p.readCharacteristic(0x15)
-  if args.verbose:
-    print(result)
-
-  #delta = str(timedelta(seconds=TimeSinceStart))
-         
 z = args.interval
-meter = args.meter         
-         
+meter = args.meter   
+
+class StatsReporter:
+    def __init__(
+        self,
+        socket_type,
+        socket_address,
+        socket_data,
+        encoding='utf-8',
+    ):
+        self._socket_type = socket_type
+        self._socket_address = socket_address
+        self._encoding = encoding
+        self._socket_data = socket_data
+        self.create_socket()
+    
+    def create_socket(self):
+        try:
+            sock = socket.socket(*self._socket_type,self._socket_data)
+            #sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+            sock.connect("/tmp/telegraf.sock")
+            self._sock = sock
+            print('Created socket')
+        except socket.error as e:
+            print(f'Got error while creating socket: {e}')
+
+    def close_socket(self):
+        try:
+            self._sock.close()
+            print('Closed socket')
+        except (AttributeError, socket.error) as e:
+            print(f'Got error while closing socket: {e}')
+    
+    def send_data(self, data):
+        try:
+            sent = self._sock.send(data.encode(self._encoding))
+            print(data)
+        except (AttributeError, socket.error) as e:
+            print(f'Got error while sending data on socket: {e}')
+            # attempt to recreate socket on error
+            self.close_socket()
+            self.create_socket()
+
+try:
+    print('attempting to connect')		
+    p = Peripheral(args.BLEaddress,addrType="random")
+except BTLEException as ex:
+    time.sleep(10)
+    print('2nd try connect')
+    p = Peripheral(args.BLEaddress,addrType="random")
+except BTLEException as ex:
+    print('cannot connect')
+    exit()
+else:
+    print('connected ',args.BLEaddress)
+
+reporter = StatsReporter(
+    (socket.AF_UNIX, ),
+    '/tmp/telegraf.sock',
+    socket.SOCK_DGRAM)
+
+atexit.register(reporter.close_socket)
+
 while True:
 	result = p.readCharacteristic(0x15)
 # Unpack into variables, skipping bytes 0-2
 	i = 3
 	PctCharged, V1Volts, V2Volts,Current, Power, Temperature, PowerMeter, ChargeMeter, TimeSinceStart, CurrentTime, PeakCurrent = struct.unpack_from('<BfffffqqIIf', result, i)
-	if args.verbose:
-		print(PctCharged, V1Volts, V2Volts, Current, Power, Temperature, PowerMeter, ChargeMeter, TimeSinceStart, Currentime, PeakCurrent)
-# Clean up vars
+	# Clean up vars
 	PctCharged = PctCharged/2
 	PowerMeter = PowerMeter/1000
 	ChargeMeter = ChargeMeter/1000
-	
-# Format and send message to socket - not sending V2Volts, TimeSinceStart, Currentime 
+
+# Format and send message to socket - not sending V2Volts, TimeSinceStart, Currentime as influxdb as timestamp
 	message = ("meter,volts,amps,watts,temp,kwh,ah,peak\r\n%s,%0.3f,%0.2f,%0.2f,%0.1f,%0.4f,%0.2f,%0.2f" % (meter,V1Volts,Current,Power,Temperature,PowerMeter,ChargeMeter,PeakCurrent))
-	print(message)
-	#sock.send(message.encode('utf8'))
+	#print(message)
+	reporter.send_data(message)
 	time.sleep(z)
 
